02fec380c6f1110ddd0d165d4890ce2ee29cbc5d
k-yomo <kanji.yy@gmail.com>
2023-02-14 23:51:13 +0900

    Fix channel parameter

--- app/lib/util/notification.dart
+++ app/lib/util/notification.dart
@@ -3,7 +3,8 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 const notificationChannel = AndroidNotificationChannel(
   'high_importance_channel', // id
   'High Importance Notifications', // title
-  'This channel is used for important notifications.', // description
+  description:
+      'This channel is used for important notifications.', // description
   importance: Importance.max,
 );
 
